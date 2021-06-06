4da13003423116fd343a9ddc31b4f3dcdef9a8b2
victorfrankenstein245 <56684147+victorfrankenstein245@users.noreply.github.com>
2021-06-06 22:18:34 +0100

    header icons

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:vista_shop_ui/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/search.svg",
+            color: kTextColor,
+          ),
+          onPressed: () {},
+        ),
+      ),
+    );
   }
 }
